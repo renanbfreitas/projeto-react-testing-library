d18faed9fc3c7323283298bedcbddd39648d955a
Renanbfreitas <renanbf1992@outlook.com>
2022-10-19 18:34:45 -0300

    Adicionando item 2 e 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,31 +4,32 @@ import About from '../pages/About';
 import renderWithRouter from './renderWithRouter';
 
 describe('Componente About.js', () => {
-    it('testa se a página contém as informações sobre a Pokédex.', () => {
-        renderWithRouter(<About />);
-        const aboutPokedex = screen.getByText('About Pokédex');
-        expect(aboutPokedex).toBeInTheDocument();
-    });
+  it('testa se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByText('About Pokédex');
+    expect(aboutPokedex).toBeInTheDocument();
+  });
 
-    it('testa se a página contém um heading h2 com o texto About Pokédex', () => {
-        renderWithRouter(<About />);
-        const aboutHeading = screen.getByRole('heading', {
-            level: 2,
-            name: /About Pokédex/i,
-        });
-        console.log(aboutHeading);
-        expect(aboutHeading).toBeInTheDocument();
+  it('testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutHeading = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
     });
+    // console.log(aboutHeading);
+    expect(aboutHeading).toBeInTheDocument();
+  });
 
-    it('testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-        renderWithRouter(<About />);
-        const aboutText = screen.getAllByText(/Pokémons/i);
-        expect(aboutText.length).toBe(2);
-    });
+  it('testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const aboutText = screen.getAllByText(/Pokémons/i);
+    expect(aboutText.length).toBe(2);
+  });
 
-    it('testa se a página contém a imagem de uma Pokédex', () => {
-        renderWithRouter(<About />);
-        const image = screen.getByRole('img');
-        expect((image).src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-    });
+  it('testa se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    // console.log(image);
+    expect((image).src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Componente FavoritePokemons.js', () => {
+  it('testa se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textNotFav = screen.getByText('No favorite pokemon found');
+    expect(textNotFav).toBeInTheDocument();
+  });
+
+  it('testa se são exibidos todos os cards de pokémons favoritados', () => {
+    const pokemonInfo = {
+      pokemName: 'Pikachu',
+      pokemType: 'Electric',
+      pokeWeight: 'Average weight: 6.0 kg',
+    };
+    const { getByText, getByLabelText } = renderWithRouter(<App />);
+    const moreInfoLink = getByText('More details');
+    expect(moreInfoLink).toBeInTheDocument();
+    fireEvent.click(moreInfoLink);
+    const pokekonfav = getByLabelText('Pokémon favoritado?');
+    fireEvent.click(pokekonfav);
+    expect(pokekonfav).toBeChecked();
+    fireEvent.click(getByText(/Favorite Pokémons/));
+    expect(screen.getByTestId('pokemon-name').textContent).toBe(pokemonInfo.pokemName);
+  });
+});
