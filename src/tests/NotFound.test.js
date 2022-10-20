dfee4036ec1d010d4e3e8224eb0400f95cb38eb3
Renanbfreitas <renanbf1992@outlook.com>
2022-10-20 10:01:14 -0300

    Adicionando item 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Componente NotFound.js', () => {
+  it('testa se a página deve ter um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByText('Page requested not found', {
+      level: 2,
+      name: /Page requested not found /i,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('testa se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img');
+    expect((image).src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
